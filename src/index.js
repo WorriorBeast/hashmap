2718f9a45e6d04dd8a603eb8a41b45349f06b93e
Emmanuel Sanchez <128549074+WorriorBeast@users.noreply.github.com>
2025-01-04 20:51:18 -0800

    Create hash map object and convert key into number

--- src/index.js
+++ src/index.js
@@ -174,3 +174,27 @@ class LinkedList {
 		}
 	}
 }
+
+class HashMap {
+	constructor() {
+		this.capacity = 16;
+		this.loadFactor = 0.75;
+		this.buckets = new Array(this.capacity);
+	}
+
+	hash(key) {
+		let hashCode = 0;
+		const primeNumber = 31;
+
+		for (let i = 0; i < key.length; i++) {
+			hashCode =
+				(primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;
+		}
+
+		return hashCode;
+	}
+}
+
+const test = new HashMap();
+
+console.log(test.hash('dog'));
